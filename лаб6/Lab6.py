24a43e22a04fe920bd28eb2c4555cf254161b596
Gorachiykakchay <124895731+Gorachiykakchay@users.noreply.github.com>
2023-05-20 01:13:48 +0400

    Update Lab6.py

--- лаб6/Lab6.py
+++ лаб6/Lab6.py
@@ -4,67 +4,62 @@
 2 часть – усложнить написанную программу, введя по своему усмотрению в условие минимум одно ограничение на характеристики объектов и целевую функцию для оптимизации решения.
 Вариант 25. У няни неограниченное количество  фруктов К разных названий (ф1,…фК). Сформировать (вывести) все возможные варианты меню полдника (N фруктов) для ребенка на неделю.
 '''
-
-from random import choice, randint
-from itertools import permutations
+import random
+from itertools import product
+from random import randint
 
 print('Введите количество разных фруктов K: ', end='')
 k = int(input())
 if k <= 0:
     print('Фрукты закончились')
     quit()
+
 while True:
     n = int(input('Введите количество фруктов в одном полднике N: '))
     if n <= 0:
         print('Некорректное количество фруктов')
-    elif n > k :
+    elif n > k:
         print(f"Недостаточно фруктов. Введите число фруктов не больше {k}")
     else:
         break
 
-print('\nПервая часть')
+print('\nЧАСТЬ 1')
+print('--------------------')
+fruits = [f'ф{i}' for i in range(1, k + 1)]
+menu_options = list(product(fruits, repeat=n))
+total_combinations = len(menu_options)
+print(f'Всего вариантов меню: {total_combinations}')
+print('Варианты меню:')
+for option in menu_options:
+    print(' '.join(option))
 
-fruits = [f'Фрукт {i}' for i in range(1, k+1)]
-days_of_week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье']
+while True:
+    days = int(input('Введите количество дней: '))
+    if days <= 0:
+        print('Некорректное количество дней')
+    else:
+        break
+        
+print('\nЧАСТЬ 2')
+print('--------------------')
 
-menu_combinations = list(permutations(fruits, n))
-total_combinations = len(menu_combinations)
-
-random_menu = [choice(menu_combinations) for i in range(len(days_of_week))]
-
-menu = []
-for i, day in enumerate(days_of_week):
-    menu_item = f'{day}: {", ".join(random_menu[i])}'
-    menu.append(menu_item)
-
-for item in menu:
-    print(item)
-
-print(f"{total_combinations} возможных комбинаций меню")
-
-print('\nВторая часть')
-
-fruits = [f'Фрукт {i}' for i in range(1, k+1)]
-days_of_week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье']
-
-menu_combinations = list(permutations(fruits, n))
-total_combinations = len(menu_combinations)
-
-random_menu = [choice(menu_combinations) for i in range(len(days_of_week))]
-
-menu = []
-for i, day in enumerate(days_of_week):
-    daily_menu = {}
-    for fruit in random_menu[i]:
-        calories = randint(1, 100)
-        daily_menu[fruit] = calories
-    max_calories_fruit = max(daily_menu, key=daily_menu.get)
-    daily_menu[f'Самый калорийный фрукт на день {day} ({max_calories_fruit}, {daily_menu[max_calories_fruit]}'] = ""
-    menu_item = f'{day}: {", ".join([f"{fruit} ({calories} ккал)" for fruit, calories in daily_menu.items()])}'
-    menu.append(menu_item)
-
-for item in menu:
-    print(item)
-
-print(f"{total_combinations} возможных комбинаций меню")
-
+for day in range(days):
+    calories = {fruit: randint(50, 150) for fruit in fruits}
+    day_combinations = menu_options[:]
+    
+    for i, option in enumerate(day_combinations):
+        random_order = list(range(n))
+        random.shuffle(random_order)
+        option = [option[j] for j in random_order]
+        option_calories = sum(calories[fruit] for fruit in option)
+        day_combinations[i] = (option, option_calories)
+    
+    day_combinations = sorted(day_combinations, key=lambda x:x[1], reverse=True)
+    max_calories_fruit = max(calories.items(), key=lambda x: x[1])
+    
+    print(f"\nМеню на день {day + 1}:")
+    for option, option_calories in day_combinations:
+        option = ' '.join(f"{fruit} ({calories[fruit]} ккал)" for fruit in option)
+        print(f"{option} ({option_calories} ккал)")
+    
+    print(f"\nСамый калорийный фрукт на день {day + 1}: {max_calories_fruit[0]} ({max_calories_fruit[1]} ккал)")
