4b556b898c40fc1cb1f9e8cd3f119515e9767da5
Gorachiykakchay <124895731+Gorachiykakchay@users.noreply.github.com>
2023-10-30 18:05:38 +0400

    Update Lab8.py

--- лаб8/Lab8.py
+++ лаб8/Lab8.py
@@ -5,49 +5,82 @@
 Вариант 25. У няни неограниченное количество  фруктов К разных названий (ф1,…фК). Сформировать (вывести) все возможные варианты меню полдника (N фруктов) для ребенка на неделю.
 '''
 import random
-import itertools
 from tkinter import *
 from tkinter import messagebox
 from tkinter import scrolledtext
 
-def combination():
-    k = int(fruits_count.get())
+class MenuGenerator:
+    def __init__(self, k):
+        self.k = k
+        self.fruits = [f'ф{i}' for i in range(1, k + 1)]
+        self.menu_options = []
 
+    def generate_menu_options(self, n):
+        for i in range(self.k ** n):
+            option = []
+            for j in range(n):
+                index = (i // (self.k ** j)) % self.k
+                option.append(self.fruits[index])
+            if len(set(option)) == n:
+                self.menu_options.append(option)
+
+    def print_menu_options(self):
+        total_combinations = len(self.menu_options)
+        results_text.insert(END, f'Всего вариантов меню: {total_combinations}\n')
+        results_text.insert(END, 'Варианты меню:\n')
+        for option in self.menu_options:
+            results_text.insert(END, ' '.join(option) + '\n')
+
+class MenuAnalyzer:
+    def __init__(self, fruits, menu_options):
+        self.fruits = fruits
+        self.menu_options = menu_options
+
+    def analyze_menu(self, days):
+        for day in range(days):
+            calories = {fruit: random.randint(50, 150) for fruit in self.fruits}
+            day_calories = []
+            for option in self.menu_options:
+                option_calories = sum(calories[fruit] for fruit in option)
+                day_calories.append((option, option_calories))
+            day_calories.sort(key=lambda x: x[1], reverse=True)
+            max_calories_fruit = max(calories.items(), key=lambda x: x[1])
+
+            results_text.insert(END, f"\nМеню на день {day + 1}:\n")
+            for option, option_calories in day_calories:
+                option = ' '.join(f"{fruit} ({calories[fruit]} ккал)" for fruit in option)
+                results_text.insert(END, f"{option} ({option_calories} ккал)\n")
+
+            results_text.insert(END, f"\nСамый калорийный фрукт на день {day + 1}: {max_calories_fruit[0]} ({max_calories_fruit[1]} ккал)\n")
+
+def combination():
+    results_text.delete(1.0, END)
+    k = int(fruits_count.get())
     if k <= 0:
         messagebox.showerror("Ошибка", "Фрукты закончились")
         return
-
     n = int(fruits_per_lunch.get())
     if n <= 0:
         messagebox.showerror("Ошибка", "Некорректное количество фруктов")
     elif n > k:
         messagebox.showerror("Ошибка", f"Недостаточно фруктов. Введите число фруктов не больше {k}")
     else:
-        results_text.delete(1.0, END)
+        menu_generator = MenuGenerator(k)
+        menu_generator.generate_menu_options(n)
+        menu_generator.print_menu_options()
 
-        fruits = [f'ф{i}' for i in range(1, k + 1)]
-        menu_options = []
-        for option in itertools.combinations(fruits, n):
-            menu_options.append(list(option))
+        while True:
+            try:
+                days = int(days_entry.get())
+                if days <= 0:
+                    messagebox.showerror("Ошибка", "Некорректное количество дней")
+                else:
+                    break
+            except ValueError:
+                messagebox.showerror("Ошибка", "Введите число дней")
 
-        days = int(days_entry.get())
-
-        for day in range(days):
-            daily_menu = menu_options
-
-            calories = {fruit: random.randint(50, 150) for fruit in fruits}
-            day_calories = []
-            for option in daily_menu:
-                option_calories = sum(calories[fruit] for fruit in option)
-                day_calories.append((option, option_calories))
-            day_calories.sort(key=lambda x: x[1], reverse=True)
-
-            results_text.insert(INSERT, f"\nМеню на день {day + 1}:\n")
-            for option, option_calories in day_calories:
-                option = ' '.join(f"{fruit} ({calories.get(fruit, 0)} ккал)" for fruit in option)
-                results_text.insert(INSERT, f"{option} ({option_calories} ккал)\n")
-        results_text.insert(INSERT, '\n')
-
+        menu_analyzer = MenuAnalyzer(menu_generator.fruits, menu_generator.menu_options)
+        menu_analyzer.analyze_menu(days)
 def click():
     window.destroy()
 
